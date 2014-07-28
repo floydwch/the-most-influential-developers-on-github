9b6c9d43d5c0e0dc7ef85aeea7185dce48b463cb
floydsoft <floydsoft@floydsoftdeMacBook-Pro.local>
2014-07-29 07:22:30 +0800

    grab first archive

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def grab():
     from urlgrabber import urlopen
     import json
 
-    url = 'http://data.githubarchive.org/2011-03-11-15.json.gz'
+    url = 'http://data.githubarchive.org/2011-02-12-0.json.gz'
 
     with GzipFile(fileobj=urlopen(url)) as gz_file:
         events = map(json.loads, list(gz_file))
