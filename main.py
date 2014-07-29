bf974998ceee9b35e167baa4da8aa8cba31e55a5
floydsoft <floydsoft@floydsoftdeMacBook-Pro.local>
2014-07-29 17:39:50 +0800

    time range for archives

--- main.py
+++ main.py
@@ -5,13 +5,24 @@ def grab():
     from gzip import GzipFile
     from urlgrabber import urlopen
     import json
+    from datetime import datetime, timedelta
 
-    url = 'http://data.githubarchive.org/2011-02-12-0.json.gz'
+    from_time = datetime(2011, 2, 12, 0)
+    to_time = datetime(2014, 7, 29, 0)
+    time_strs = [(from_time + timedelta(hours=x)).strftime(
+        '%Y-%m-%d-%-H') for x in range(0, int(
+            (to_time - from_time).total_seconds() / 3600))]
 
-    with GzipFile(fileobj=urlopen(url)) as gz_file:
-        events = map(json.loads, list(gz_file))
-        watch_events = filter(lambda x: x['type'] == 'WatchEvent', events)
-        print watch_events
+    for time_str in time_strs:
+        url = 'http://data.githubarchive.org/%s.json.gz' % time_str
+
+        with GzipFile(fileobj=urlopen(url)) as gz_file:
+            events = map(json.loads, list(gz_file))
+            watch_events = map(
+                lambda x: x,
+                filter(lambda x: x['type'] == 'WatchEvent', events))
+
+            # print (map(lambda x: x[1].get('login', False), watch_events))
 
 
 grab()
