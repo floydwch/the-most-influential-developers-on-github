e3d521c7edbed25ce99b5ffa138c17f7f12fc082
floydsoft <floydsoft@floydsoftdeMacBook-Pro.local>
2014-07-30 16:44:14 +0800

    extract grabbing numbers from grab

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
+
+FROM_TIME = datetime(2011, 2, 12, 0)
+TO_TIME = datetime(2014, 7, 29, 0)
+
 
 def loads_invalid_obj_list(s):
     from json import JSONDecoder
@@ -32,35 +37,33 @@ def field_select(event):
             event.get('created_at', None))
 
 
-def grab():
+def grab(number):
     from gzip import GzipFile
     from urlgrabber import urlopen
-    from datetime import datetime, timedelta
+    from datetime import timedelta
 
-    from_time = datetime(2011, 2, 12, 0)
-    to_time = datetime(2014, 7, 29, 0)
-    time_strs = [(from_time + timedelta(hours=x)).strftime(
-        '%Y-%m-%d-%-H') for x in range(0, int(
-            (to_time - from_time).total_seconds() / 3600))]
+    time_str = (FROM_TIME + timedelta(hours=number)).strftime('%Y-%m-%d-%-H')
 
-    for time_str in time_strs:
-        url = 'http://data.githubarchive.org/%s.json.gz' % time_str
+    # for time_str in time_strs:
+    url = 'http://data.githubarchive.org/%s.json.gz' % time_str
 
-        while True:
-            try:
-                with GzipFile(fileobj=urlopen(url)) as gz_file:
-                    events = loads_invalid_obj_list(''.join(
-                        map(lambda x: unicode(x, 'ISO-8859-1'), map(
-                            lambda x: x.strip(), list(gz_file)))))
+    while True:
+        try:
+            with GzipFile(fileobj=urlopen(url)) as gz_file:
+                events = loads_invalid_obj_list(''.join(
+                    map(lambda x: unicode(x, 'ISO-8859-1'), map(
+                        lambda x: x.strip(), list(gz_file)))))
 
-                    watch_events = map(field_select, filter(
-                        lambda x: x['type'] == 'WatchEvent', events))
+                watch_events = map(field_select, filter(
+                    lambda x: x['type'] == 'WatchEvent', events))
 
-                    print watch_events, time_str
-            except Exception as e:
-                print e
-                continue
-            break
+                print watch_events, time_str
+        except Exception as e:
+            print e
+            continue
+        break
 
 
-grab()
+numbers = range(int((TO_TIME - FROM_TIME).total_seconds() / 3600))
+
+map(grab, numbers)
