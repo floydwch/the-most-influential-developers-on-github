5375fc95a29af336525e4da3cf1e9e8e2f88b269
floydsoft <floydsoft@vitas.local>
2014-08-07 02:26:37 +0800

    extract stargazers

--- task_project_repos.py
+++ task_project_repos.py
@@ -8,6 +8,13 @@ db = client['github']
 watch_events = db['watch_events']
 
 db.watch_events.aggregate([
-    {'$group': {'_id': '$repo'}},
-    {'$project': {'_id': False, 'name': '$_id'}},
-    {'$out': 'repos'}])
+    {'$group': {
+        '_id': '$repo',
+        'stargazers': {
+            '$push': {
+                'actor': '$actor',
+                'created_at': '$created_at'
+            }
+        }}},
+    {'$project': {'_id': False, 'name': '$_id', 'stargazers': True}},
+    {'$out': 'repos'}], allowDiskUse=True)
