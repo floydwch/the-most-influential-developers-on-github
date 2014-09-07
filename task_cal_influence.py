2d3ce4e7b8b11f95e9f435355934e55959a2f43c
floydsoft <floydsoft@gmail.com>
2014-09-07 10:35:53 +0800

    update task_cal_influence

--- task_cal_influence.py
+++ task_cal_influence.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from underscore import _ as us
-from pymongo import MongoClient, ASCENDING
+from pymongo import MongoClient
 import operator
 from multiprocessing import Pool
 import gc
@@ -9,72 +9,63 @@ import gc
 
 gc.disable()
 
-client = MongoClient()
-db = client['github']
-pageranks = db['pageranks']
-influences = db['influences']
-
-
-# def max_record(records):
-#     return max(records, key=lambda x: x['pagerank'])
-
 
 def cal_influence_ranking((actor, records)):
-    # return (actor,
-    #         sum(filter(lambda x: x > 1, us.pluck(
-    #             map(max_record,
-    #                 group_by(lambda x: x['repo'], records).values()),
-    #             'pagerank'))))
     return (actor, sum(filter(lambda x: x > 1, us.pluck(records, 'pagerank'))))
 
 
-def influence_ranks(spec):
-    pool = Pool()
-    ranks = sorted(
-        pool.map(cal_influence_ranking, us.groupBy(
-            list(pageranks.find(spec)), 'actor').items()),
-        key=operator.itemgetter(1), reverse=True)
-
-    pool.close()
-    pool.join()
-
-    return ranks
-
-
-influence_specs = {
-    'General': {},
-    'JavaScript': {'language': 'JavaScript'},
-    'CSS': {'language': 'CSS'},
-    'Python': {'language': 'Python'},
-    'Ruby': {'language': 'Ruby'},
-    'Go': {'language': 'Go'},
-    'PHP': {'language': 'PHP'},
-    'Shell': {'language': 'Shell'},
-    'Perl': {'language': 'Perl'},
-    'Objective-C': {'language': 'Objective-C'},
-    'Swift': {'language': 'Swift'},
-    'Java': {'language': 'Java'},
-    'C++': {'language': 'C++'},
-    'C#': {'language': 'C#'},
-    'C': {'language': 'C'},
-    'Haskell': {'language': 'Haskell'},
-    'Scala': {'language': 'Scala'},
-    'Erlang': {'language': 'Erlang'},
-    'Clojure': {'language': 'Clojure'}
-}
-
-
 def main():
-    for field, spec in influence_specs.items():
-        ranks = influences.find({'field': field}).sort('ranks', ASCENDING)
+    client = MongoClient()
+    db = client['github']
+    pageranks = db['pageranks']
+    influences = db['influences']
 
-        if not ranks:
+    def influence_ranks(spec):
+        pool = Pool(15)
+        ranks = sorted(
+            pool.map(cal_influence_ranking, us.groupBy(
+                list(pageranks.find(spec)), 'actor').items()),
+            key=operator.itemgetter(1), reverse=True)
+
+        pool.close()
+        pool.join()
+
+        return ranks
+
+    influence_specs = {
+        'General': {},
+        'JavaScript': {'language': 'JavaScript'},
+        'CSS': {'language': 'CSS'},
+        'Python': {'language': 'Python'},
+        'Ruby': {'language': 'Ruby'},
+        'Go': {'language': 'Go'},
+        'PHP': {'language': 'PHP'},
+        'Shell': {'language': 'Shell'},
+        'Perl': {'language': 'Perl'},
+        'Objective-C': {'language': 'Objective-C'},
+        'Swift': {'language': 'Swift'},
+        'Java': {'language': 'Java'},
+        'C++': {'language': 'C++'},
+        'C#': {'language': 'C#'},
+        'C': {'language': 'C'},
+        'Haskell': {'language': 'Haskell'},
+        'Scala': {'language': 'Scala'},
+        'Erlang': {'language': 'Erlang'},
+        'Clojure': {'language': 'Clojure'}
+    }
+
+    for field, spec in influence_specs.items():
+        influence = influences.find_one({'field': field})
+
+        if not influence:
             ranks = influence_ranks(spec)
 
             influences.insert({
                 'field': field,
-                'ranks': ranks
+                'ranks': ranks[:1000]
             })
+        else:
+            ranks = influence['ranks']
 
         print '%s top 25:' % field
         for i, (actor, pagerank) in enumerate(ranks[:25]):
