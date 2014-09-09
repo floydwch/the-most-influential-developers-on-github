a61f3f091dd9c6a8806d38029c9acd4de25b3423
floydsoft <floydsoft@gmail.com>
2014-09-09 14:33:14 +0800

    consider the ownership

--- task_draw_histogram.py
+++ task_draw_histogram.py
@@ -48,10 +48,15 @@ def main(field):
 
         return number
 
+    def count_public_repos(user):
+        return github.get_user(user).public_repos
+
     followers_numbers = map(count_followers, ranks)
     stars_numbers = map(count_stars, ranks)
+    public_repos_numbers = map(count_public_repos, ranks)
     products_sqrts = map(math.sqrt, list(starmap(
-        operator.mul, zip(followers_numbers, stars_numbers))))
+        operator.mul, zip(followers_numbers, list(starmap(
+            operator.add, zip(stars_numbers, public_repos_numbers)))))))
 
     fig, ax = plt.subplots()
 
@@ -61,15 +66,17 @@ def main(field):
     bar_width = 0.2
 
     plt.bar(index, followers_numbers, bar_width, color='r', label='Followers')
-    plt.bar(index + bar_width, stars_numbers, bar_width, color='b',
+    plt.bar(index + bar_width, stars_numbers, bar_width, color='g',
             label='Starred')
-    plt.bar(index + 2 * bar_width, products_sqrts, bar_width, color='y',
+    plt.bar(index + 2 * bar_width, public_repos_numbers, bar_width, color='b',
+            label='Repos')
+    plt.bar(index + 3 * bar_width, products_sqrts, bar_width, color='y',
             label='Product')
 
     plt.xlabel('Developer')
     plt.ylabel('Number')
-    plt.title("Developer's Followers and Starred")
-    plt.xticks(index + 2 * bar_width, ranks)
+    plt.title("Developer's Followers, Starred and Repos")
+    plt.xticks(index + 3 * bar_width, ranks)
     plt.legend()
     plt.tight_layout()
 
